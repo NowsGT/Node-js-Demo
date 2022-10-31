0db57879eb17a9d6ba9c64808b138c6ba7ffbc91
Nowsathk <nowsath_uom@ymail.com>
2022-11-01 00:32:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('Hello, Node js');
 console.log('Testing from "main"');
-
+console.log('yml file changed');
 /** var moment = require('moment');
 var date = moment().format('LL');
 console.log(date);
