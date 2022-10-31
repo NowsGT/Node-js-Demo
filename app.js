5b3fdc74da75e578d4310d28654eca1b58d31187
Nowsathk <nowsath_uom@ymail.com>
2022-11-01 00:22:02 +0530

    add, for minor changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log('Hello, Node js');
 console.log('Testing from "main"');
 console.log('added target branch');
-/**var moment = require('moment');
+/** var moment = require('moment');
 var date = moment().format('LL');
 console.log(date);
 **/
