6c9fb7540ecdcb7c65be187105651a5afa3abffb
Nowsathk <nowsath_uom@ymail.com>
2022-11-01 00:18:31 +0530

    Added target branch in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('Hello, Node js');
 console.log('Testing from "main"');
-console.log('add');
+console.log('added target branch');
 /**var moment = require('moment');
 var date = moment().format('LL');
 console.log(date);
