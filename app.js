dee253e2a534a019b54a9ce7c5a39b72880aa7be
Nowsathk <nowsath_uom@ymail.com>
2022-11-01 08:48:10 +0530

    [minor, added] wording added in main_deploy.yml

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('Hello, Node js..');
 console.log('Testing from "main.."');
-console.log('Checking on 1.1.1');
+console.log('Added wording...');
 /** var moment = require('moment');
 var date = moment().format('LL');
 console.log(date);
