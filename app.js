40c46e2d596b609ce1a3029b9a24f33dc6721cc2
Nowsathk <nowsath_uom@ymail.com>
2022-11-01 09:10:04 +0530

    [minor] removed env values

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log('Hello, Node js..');
 console.log('Testing from "main.."');
 console.log('Added wording...');
 console.log('Set to staging...');
+console.log('Removed env variable');
 /** var moment = require('moment');
 var date = moment().format('LL');
 console.log(date);
