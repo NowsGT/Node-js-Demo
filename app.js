88c64d55c07e22765f3abb8cc706f22fc77804b8
Nowsathk <nowsath_uom@ymail.com>
2022-11-01 09:27:21 +0530

    [minor] paths ignored in main_deploy.yml

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log('Testing from "main.."');
 console.log('Added wording...');
 console.log('Set to staging...');
 console.log('Removed env variables..');
+console.log('package.json ignored..');
 /** var moment = require('moment');
 var date = moment().format('LL');
 console.log(date);
