7b6ce3f85259b6ef4853516ab332b59dd0e8294b
Nowsathk <nowsath_uom@ymail.com>
2022-11-01 08:52:20 +0530

    [patch] environment chaged to staging

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log('Hello, Node js..');
 console.log('Testing from "main.."');
 console.log('Added wording...');
+console.log('Set to staging...');
 /** var moment = require('moment');
 var date = moment().format('LL');
 console.log(date);
