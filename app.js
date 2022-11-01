2b087c544ebd357eb7947c69336b0df17dd1af5d
Nowsathk <nowsath_uom@ymail.com>
2022-11-01 09:13:09 +0530

    [minor] app.js changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('Hello, Node js..');
 console.log('Testing from "main.."');
 console.log('Added wording...');
 console.log('Set to staging...');
-console.log('Removed env variable');
+console.log('Removed env variables..');
 /** var moment = require('moment');
 var date = moment().format('LL');
 console.log(date);
