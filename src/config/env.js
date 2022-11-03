4d9b479736c07fe6a572e3e4faae7650fb56056b
Nowsathk <nowsath_uom@ymail.com>
2022-11-03 10:22:15 +0530

    Create env.js

---
+++ src/config/env.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    API_ANALYTICS_BASE_URL: 'https://xxxxxxxxxxxxxxxxxxx.com',
+    API_APP_BASE_URL: 'https://xxxxxxxxxxxxxxxxxxx.com',
+    API_BASE_URL: 'https://xxxxxxxxxxxxxxxxxxx.com',
+    API_PUSH_BASE_URL: 'https://xxxxxxxxxxxxxxxxxxx.com',
+    APP_CONTENT_URL: 'https://xxxxxxxxxxxxxxxxxxx.com',
+    ICON_BUCKET: 'https://xxxxxxxxxxxxxxxxxxx.com',
+    IMAGE_BUCKET: 'https://xxxxxxxxxxxxxxxxxxx.com',
+    IS_STAGING: false,
+    MAPBOX_API_TOKEN: 'fdsfdslfjlsdjg;ldsj2312423423412',
+    USE_MOCK_SERVICE: false,
+    VERSION: '0.17.0',
+  },
+};
